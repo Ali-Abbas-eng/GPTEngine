091ed43033ab77798ff04c030fc292a7b6b2a01b
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-02-15 13:04:07 +0300

    [ESP-12] dummy response to input audio

--- ai_request_handlers.py
+++ ai_request_handlers.py
@@ -1,19 +1,25 @@
 import random
-
+from io import BytesIO
 import assemblyai as aai
 import os
 import boto3
 from typing import Union
 import openai
-from globals import (
-    API_KEY,
+from .globals import (
+    OPENAI_API_KEY,
     IELTS_PART_1_PROMPT,
     IELTS_PART_2_PROMPT,
     IELTS_PART_3_PROMPT,
     IELTS_PART_1_DUMMY_START_TRIGGER,
     IELTS_PART_2_DUMMY_START_TRIGGER,
     IELTS_PART_3_DUMMY_START_TRIGGER,
-    IELTS_POSSIBLE_TOPICS_LIST
+    IELTS_POSSIBLE_TOPICS_LIST,
+    AWS_API_KEY,
+    AWS_ACCESS_KEY_ID,
+    AWS_REGION_NAME,
+    AWS_POLY_VOICE_ID,
+    AWS_POLY_OUTPUT_FORMAT,
+    AWS_POLY_OUTPUT_DIRECTORY
 )
 
 
@@ -29,12 +35,12 @@ class TextToSpeechEngine:
         text_to_speech_converter("text you'd like to here read aloud", file_id) -> audio file in specified output dir
     """
     def __init__(self,
-                 secret_access_key: Union[str, os.PathLike] = os.path.join('secret', 'AWS', 'SECRET_ACCESS_KEY.txt'),
-                 access_key_id: Union[str, os.PathLike] = os.path.join('secret', 'AWS', 'ACCESS_KEY_ID.txt'),
-                 region_name_file: Union[str, os.PathLike] = os.path.join('secret', 'AWS', 'REGION_NAME.txt'),
-                 voice_id: str = 'Emma',
-                 output_format: str = 'mp3',
-                 output_directory: Union[str, os.PathLike] = os.path.join('output', 'audios')):
+                 secret_access_key: Union[str, os.PathLike] = AWS_API_KEY,
+                 access_key_id: Union[str, os.PathLike] = AWS_ACCESS_KEY_ID,
+                 region_name_file: Union[str, os.PathLike] = AWS_REGION_NAME,
+                 voice_id: str = AWS_POLY_VOICE_ID,
+                 output_format: str = AWS_POLY_OUTPUT_FORMAT,
+                 output_directory: Union[str, os.PathLike] = AWS_POLY_OUTPUT_DIRECTORY):
         """
         initialise the Text to Speech Engine with the variable representing the Amazon Polly Account Configurations.
         :param secret_access_key: str or os.PathLike, /path/to/SECRET_ACCESS_KEY.txt.
@@ -49,9 +55,9 @@ class TextToSpeechEngine:
         self.output_directory = output_directory
         os.makedirs(self.output_directory, exist_ok=True)
         self.polly_service_handle = boto3.client('polly',
-                                                 region_name=open(region_name_file).read(),
-                                                 aws_access_key_id=open(access_key_id).read(),
-                                                 aws_secret_access_key=open(secret_access_key).read())
+                                                 region_name=region_name_file,
+                                                 aws_access_key_id=access_key_id,
+                                                 aws_secret_access_key=secret_access_key)
 
     def __call__(self, text, file_id):
         """
@@ -59,17 +65,20 @@ class TextToSpeechEngine:
         given text, the call will generate an audio file at specified_output_directory/{file_id}.{outputformat}
         :param text: str, the string to be read aloud.
         :param file_id: int, a unique number to be assigned to the file (the server should save it to access it later).
+        :param write: bool, whether to write the audio to a file (the server should save it to access it).
         :return:
         """
         aws_polly_service_response = self.polly_service_handle.synthesize_speech(Text=text,
                                                                                  OutputFormat=self.output_format,
                                                                                  VoiceId=self.voice_id)
         file_path = os.path.join(self.output_directory, f'{file_id}.{self.output_format}')
+        print("I'm trying to write the audio")
         if "AudioStream" in aws_polly_service_response:
             with aws_polly_service_response['AudioStream'] as audio_stream:
+                audio_data = BytesIO(audio_stream.read())
                 try:
                     with open(file_path, 'wb') as file_output_handle:
-                        file_output_handle.write(audio_stream.read())
+                        file_output_handle.write(audio_data.getvalue())
                         file_output_handle.close()
                     return file_path
                 except IOError as error:
@@ -81,8 +90,8 @@ class TextToSpeechEngine:
 
 
 class SpeechToTextEngine:
-    def __init__(self, api_key: Union[str, os.PathLike] = os.path.join('secret', 'AssemblyAI/API_KEY.txt')):
-        aai.settings.api_key = open(api_key).read()
+    def __init__(self, api_key: Union[str, os.PathLike] = OPENAI_API_KEY):
+        aai.settings.api_key = api_key
         self.assembly_ai_service_handle = aai.Transcriber()
 
     def __call__(self, file_url, *args, **kwargs):
@@ -105,7 +114,7 @@ class ChatGPTIELTSExaminer:
         """
         Initializes the ChatGPTIELTSExaminer class.
         """
-        openai.api_key = API_KEY
+        openai.api_key = OPENAI_API_KEY
         self.session_topic = random.choice(IELTS_POSSIBLE_TOPICS_LIST)
         self.part_prompts = [
             IELTS_PART_1_PROMPT,
@@ -185,6 +194,50 @@ class ChatGPTIELTSExaminer:
             }
         )
         return self.get_chat_gpt_response()
+
+
+class BaseConversationalist:
+    """
+    This class simulates a general speaking assistant using OpenAI's GPT-3.5 model.
+    """
+
+    def __init__(self, media_root):
+        """
+        Initializes the ChatGPTIELTSExaminer class.
+        """
+        openai.api_key = OPENAI_API_KEY
+        self.speech_to_text_engine = SpeechToTextEngine()
+        self.text_to_speech_engine = TextToSpeechEngine()
+        self.media_root = media_root
+        self.chat_history = []
+
+    def __call__(self, audio_bytes, message_id, *args, **kwargs):
+        """
+        Appends the user's response to the chat history and returns the response from get_chat_gpt_response.
+
+        Args:
+            audio_bytes: binary data representing the input voice message.
+            message_id: unique identifier for the file to be written to disk
+        Returns:
+            str: The response from the GPT-3.5 model.
+        """
+        audio_file_path = os.path.join(self.media_root, 'audios')
+        os.makedirs(audio_file_path, exist_ok=True)
+        audio_file_path = os.path.join(audio_file_path, str(message_id) + '.wav')
+        with open(audio_file_path, 'wb') as audio_file:
+            audio_file.write(audio_bytes)
+            audio_file.close()
+        transcript = self.speech_to_text_engine(file_url=audio_file_path)
+        self.chat_history.append(transcript)
+        text_response = "This is a dummy response, later to be replaced with ChatGPT responses."
+        audio_response = self.text_to_speech_engine(text_response, file_id=message_id)
+        audio_url = 'http://127.0.0.1:8000/media/audios/' + str(message_id) + '.wav'
+        return {
+            'user_transcript': transcript,
+            'gpt_response': text_response,
+            'audio_response': audio_response,
+            'audio_url': audio_url
+        }
 
 
 def test_text_to_speech():
