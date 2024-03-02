bb22dab500fbd5a1e542ac084e7705f435fa396d
Ali-Abbas-Personal <ali.abbas.ssj1999@gmail.com>
2024-03-02 17:55:47 +0300

    [ESP-31] removed depricated files

--- globals.py
+++ globals.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,16 +12,5 @@ IELTS_PART_2_PROMPT = open(IELTS_PART_2_PROMPT_FILE).read()
 IELTS_PART_3_PROMPT_FILE = os.path.join(BASE_DIR, 'conversationalists/prompts', 'IELTS', 'PART_3_SYSTEM_PROMPT.txt')
 IELTS_PART_3_PROMPT = open(IELTS_PART_3_PROMPT_FILE).read()
 
-IELTS_PART_1_DUMMY_START_TRIGGER_FILE = os.path.join(BASE_DIR, 'conversationalists/prompts', 'IELTS', 'PART_1_DUMMY_START_TRIGGER.txt')
-IELTS_PART_1_DUMMY_START_TRIGGER: str = open(IELTS_PART_1_DUMMY_START_TRIGGER_FILE).read()
-
-IELTS_PART_2_DUMMY_START_TRIGGER_FILE = os.path.join(BASE_DIR, 'conversationalists/prompts', 'IELTS', 'PART_2_DUMMY_START_TRIGGER.txt')
-IELTS_PART_2_DUMMY_START_TRIGGER: str = open(IELTS_PART_2_DUMMY_START_TRIGGER_FILE).read()
-
-IELTS_PART_3_DUMMY_START_TRIGGER_FILE = os.path.join(BASE_DIR, 'conversationalists/prompts', 'IELTS', 'PART_3_DUMMY_START_TRIGGER.txt')
-IELTS_PART_3_DUMMY_START_TRIGGER: str = open(IELTS_PART_3_DUMMY_START_TRIGGER_FILE).read()
-
 IELTS_POSSIBLE_TOPICS_LIST_FILE = os.path.join(BASE_DIR, 'conversationalists/prompts', 'IELTS', 'IELTS_POSSIBLE_TOPICS.json')
-IELTS_POSSIBLE_TOPICS_LIST = list(eval(open(IELTS_POSSIBLE_TOPICS_LIST_FILE).read()).values())
-
-END_OF_IELTS_SPEAKING_SESSION = "Thank you for taking the IELTS test"
+IELTS_POSSIBLE_TOPICS_LIST = json.load(open(IELTS_POSSIBLE_TOPICS_LIST_FILE))
